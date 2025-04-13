7dd6219867fecddfb4fb12ea5eabbaf42582201f
TypeTew <tewsk132@gmail.com>
2025-04-14 01:25:09 +0700

    remove commented code and adjust footer component import in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 // import { RouterLink } from '@angular/router';
-// import { HeaderComponent } from "../shared/header/header.component";
-import { FooterComponent } from "../shared/footer/footer.component";
 
 
 @Component({
   selector: 'app-home',
-  imports: [ FooterComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
