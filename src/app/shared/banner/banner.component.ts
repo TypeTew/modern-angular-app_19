94dd88fe6582fcc71609666909443974941da411
TypeTew <tewsk132@gmail.com>
2025-04-14 00:24:40 +0700

    การส่งค่าโดยใช้ signal แบบข้าม  banner

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
+  title = signal("Welcome to banner");
+  isToggle = signal(false);
+    banner = signal({
+      id: 1,
+      title: 'Coke'
+    });
+
+  updateData(): void {
+    this.title.set("Hello signals");
+    this.isToggle.update(value => !value);
+      this.banner.update((value) => (
+        {
+          ...value,
+          title: 'Pepsi'
+        }
+      ));
+
+  }
+
 
 }
