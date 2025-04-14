3c62273c7e6fe2d49bce88592d7c558748230aa5
TypeTew <tewsk132@gmail.com>
2025-04-14 18:04:10 +0700

    clean code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AboutComponent } from './about/about.component';
 import { productRoutes } from './products/product.routes';
-import { dashboardRoutes } from './dashboards/dashboard.routes';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, title: 'หน้าหลัก' },
