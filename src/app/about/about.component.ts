2b2f1f4046f2e172266f806a9279ceca704c9e4e
TypeTew <tewsk132@gmail.com>
2025-04-27 00:57:07 +0700

    service add  api about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,19 +1,26 @@
 import { Component, effect, inject } from '@angular/core';
 import { TitleService } from '../shared/title.service';
+import { AppUtilService } from '../shared/app-util.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [AsyncPipe, JsonPipe],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
   private readonly titleService = inject(TitleService);
 
+  private readonly appUtilService = inject(AppUtilService);
+  versionInfo$!: Observable<any>;
+
   constructor() {
     effect(() => {
       this.titleService.setTitle('new about us');
-      console.log('AboutComponent initialized');
+      this.versionInfo$ = this.appUtilService.getAPIVersion();
+      //console.log('AboutComponent initialized');
     })
   }
 
